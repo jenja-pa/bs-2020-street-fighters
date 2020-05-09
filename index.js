3c6241cc95caaf0c519c3df5109338c3952c971e
Evgen Panasko <jenja.pa@gmail.com>
2020-05-09 23:08:23 +0300

    Code transform to function with try ... catch ... finally

--- index.js
+++ index.js
@@ -2,6 +2,47 @@ const API_URL = 'https://api.github.com/';
 
 var rootEl = document.getElementById("root");
 var loadEl = document.getElementById("loading-overlay");
+
+async function startApp() {
+  try {
+    loadEl.style.visibility = 'visible';
+    
+    const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
+    const fighters = await callApi(endpoint, 'GET');
+    
+    rootEl.innerText = getFightersNames(fighters);
+  } catch (error) {
+    console.warn(error);
+    rootEl.innerText = 'Failed to load data';
+  } finally {
+    loadEl.style.visibility = 'hidden';
+  }
+}
+
+function callApi(endpoind, method) {
+  const url = API_URL + endpoind
+  const options = {
+    method
+  };
+
+  return fetch(url, options)
+    .then(response => 
+      response.ok 
+        ? response.json() 
+        : Promise.reject(Error('Failed to load'))
+    )
+    .then(file => JSON.parse(atob(file.content)))
+    .catch(error => { throw error });
+}
+
+
+
+
+/*
+const API_URL = 'https://api.github.com/';
+
+var rootEl = document.getElementById("root");
+var loadEl = document.getElementById("loading-overlay");
 loadEl.style.visibility = "visible";
 
 const startApp = function() {
@@ -31,6 +72,8 @@ function callApi(endpoind, method) {
       loadEl.style.visibility = "";
     });
 }
+
+*/
 
 function getFightersNames(fighters) {
   const names = fighters.map(it => it.name).join('\n');
