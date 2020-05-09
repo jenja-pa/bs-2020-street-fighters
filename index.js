3c17a4272b923c6a59b97fc4b37255b3e8a69283
Evgen Panasko <jenja.pa@gmail.com>
2020-05-09 20:30:33 +0300

    Add chain Promises .catch .finally and use logo to show process load

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 
 var rootEl = document.getElementById("root");
-rootEl.innerText = "Loading ...";
+var loadEl = document.getElementById("loading-overlay");
+loadEl.style.visibility = "visible";
+
+//rootEl.innerText = "Loading ...";
 
 fetch(API_URL)
-	.then( resp => resp.json() )
+	.then( resp => {
+		console.log(resp);
+		if(!resp.ok){
+			return Promise.reject(new Error("Not Good we do not get correct response. from: "+ API_URL));
+		}
+		return resp.json();})
 	.then( file => {
+		console.log(file);
 		const fighters = JSON.parse(atob(file.content));
 		const names =  fighters.map(el => el.name).join("\n");
 		rootEl.innerText = names;
+	})
+	.catch(err => {
+		console.warn(err);
+		rootEl.innerText = err;
+	})
+	.finally(()=>{
+		loadEl.style.visibility = "";
 	});
