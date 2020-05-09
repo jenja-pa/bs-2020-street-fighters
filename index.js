ac3bf9d0cc384fa6c99ae0d3cf21b6ab8188f24a
Evgen Panasko <jenja.pa@gmail.com>
2020-05-09 18:53:12 +0300

    Output names fighters to content of browser

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
-/*
-const respPromise = fetch(API_URL);
 
-respPromise.then(resp => { console.log(resp.json()) });
-*/ 
+var rootEl = document.getElementById("root");
+rootEl.innerText = "Loading ...";
 
 fetch(API_URL)
 	.then( resp => resp.json() )
 	.then( file => {
 		const fighters = JSON.parse(atob(file.content));
 		const names =  fighters.map(el => el.name).join("\n");
-		console.log(names);
+		rootEl.innerText = names;
 	});
