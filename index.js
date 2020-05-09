d490e6ff62cf64e8f22410020202b8ac59bff4c1
Evgen Panasko <jenja.pa@gmail.com>
2020-05-09 18:27:23 +0300

    First  [3;2~

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+/*
+const respPromise = fetch(API_URL);
 
-const responsePromise = fetch(API_URL);
-responsePromise.then(response => { response.json() });
+respPromise.then(resp => { console.log(resp.json()) });
+*/ 
+
+fetch(API_URL)
+	.then( resp => resp.json() )
+	.then( file => {
+		const fighters = JSON.parse(atob(file.content));
+		console.log(fighters);
+	});
